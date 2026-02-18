3d7dd9d880073ff46632e7efbcfa45142bd6d52a
Giona Imperatori <giona.imperatori@sensirion.com>
2026-02-18 17:05:18 +0100

    feat: add Cargo project

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+///! This is a library for parsing and verifying mcuboot images as per the documentation at https://docs.mcuboot.com/design.html#image-format
+
+pub fn parse_image(path: &std::path::PathBuf) { }
